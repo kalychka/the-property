8e3c1f6942b609789eed177a6453c8a17cd3eacb
kolychka <randytaylor.fckoff@gmail.com>
2020-09-27 00:52:04 +0500

    Added reviews and corusel

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
+
+
 import './assets/font.css'
 import './assets/fontawesome/css/all.css'
 import './styles/style.scss'
 import './index.pug'
 
 import './components/button/button.js'
+
+import './components/review/review.js'
+
+import '../node_modules/jquery/dist/jquery.js'
+import './assets/slick-1.8.1/slick/slick.js'
+import './assets/slick-1.8.1/slick/slick.css'
+
+$(document).ready(function(){
+    $('#reviews').slick({
+        infinite: true,
+        speed: 300,
+        slidesToScroll: 1,
+        prevArrow: `<div class="prevButton"> <i class="fas fa-long-arrow-alt-left"></i> </div>`,
+        nextArrow: `<div class="nextButton"> <i class="fas fa-long-arrow-alt-right"></i> </div>`,
+        centerMode: true,
+        variableWidth: true,
+        responsive: [
+            {
+              breakpoint: 820,
+              settings: {
+                centerMode: false
+              }
+            },
+        ]
+    });
+  });
