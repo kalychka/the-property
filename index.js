e965028fd80383757dcea34ff206b87b72feecf0
kolychka <randytaylor.fckoff@gmail.com>
2020-09-29 12:20:16 +0500

    Added parallax

--- index.js
+++ index.js
@@ -45,4 +45,19 @@ $(document).ready(function(){
             }
         ]
     });
+    $('.offer').on('mousemove', (e) => {
+      const x = e.pageX / $(window).width();
+      const y = e.pageY / $(window).height();
+
+      $('.offer-parallax__star ').css(
+        'transform',
+        'translate(' + x * 60  + 'px, ' + y * 60 + 'px)' 
+      )
+
+      $('.offer-parallax__ellipse').css(
+        'transform',
+        'translate(-' + x * 30  + 'px, -' + y * 30 + 'px)' 
+      )
+
+    })
   });
