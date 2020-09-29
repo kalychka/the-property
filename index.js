1a37a7c3a00625058fb4fcea353c6f4ec79b5444
kolychka <randytaylor.fckoff@gmail.com>
2020-09-29 10:14:10 +0500

    Added and revrite to bootstrap

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import './components/loader/loader.js'
 
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+
 import './assets/font.css'
 import './assets/fontawesome/css/all.css'
 import './styles/style.scss'
@@ -15,6 +18,8 @@ import '../node_modules/jquery/dist/jquery.js'
 import './assets/slick-1.8.1/slick/slick.js'
 import './assets/slick-1.8.1/slick/slick.css'
 
+
+
 $(document).ready(function(){
     $('#reviews').slick({
         
@@ -22,7 +27,7 @@ $(document).ready(function(){
         autoplay: true,
         autoplaySpeed: 1800,
         speed: 600,
-        slidesToShow: 2,
+        slidesToShow: 1,
         slidesToScroll: 1,
         prevArrow: `<div class="prevButton"> <i class="fas fa-long-arrow-alt-left"></i> </div>`,
         nextArrow: `<div class="nextButton"> <i class="fas fa-long-arrow-alt-right"></i> </div>`,
