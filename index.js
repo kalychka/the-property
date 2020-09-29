23f6d963f223a4ed542f6e05341f345ceb0989cb
kolychka <randytaylor.fckoff@gmail.com>
2020-09-29 22:07:10 +0500

    Added catalog and photo viewer

--- index.js
+++ index.js
@@ -22,9 +22,8 @@ import './assets/slick-1.8.1/slick/slick.css'
 
 
 
-$(document).ready(function(){
-    $('#reviews').slick({
-        
+$(function() {
+    $('#reviews').slick({    
         infinite: true,
         autoplay: true,
         autoplaySpeed: 1800,
@@ -33,18 +32,8 @@ $(document).ready(function(){
         slidesToScroll: 1,
         prevArrow: `<div class="prevButton"> <i class="fas fa-long-arrow-alt-left"></i> </div>`,
         nextArrow: `<div class="nextButton"> <i class="fas fa-long-arrow-alt-right"></i> </div>`,
-        centerMode: true,
-        variableWidth: false,
-        responsive: [
-            {
-              breakpoint: 1100,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-              }
-            }
-        ]
     });
+
     $('.offer').on('mousemove', (e) => {
       const x = e.pageX / $(window).width();
       const y = e.pageY / $(window).height();
@@ -58,6 +47,24 @@ $(document).ready(function(){
         'transform',
         'translate(-' + x * 30  + 'px, -' + y * 30 + 'px)' 
       )
+    });
 
+    $('.card').on('click', function() {
+      const src = $(this).find('img').attr('src');
+      $('body').append(' <div id="overlay"></div> <div id="property-item__modal"> <img src="'+src+'"> </div>');
+      $('#property-item__modal').css({
+        left: ( $(document).width() - $('#property-item__modal').outerWidth() ),
+        top: ( $(window).height() - $('#property-item__modal').outerHeight() )
+      })
+      $('#property-item__modal').fadeIn();
+    });
+
+    $('body').on('click', '#property-item__modal', function(event) {
+      event.preventDefault();
+      $('#property-item__modal, #overlay').fadeOut('fast', function() {
+        $('#property-item__modal, #overlay').remove();
+      })
     })
+    
+
   });
